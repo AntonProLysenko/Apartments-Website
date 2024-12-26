97c16ac61846f2454581bd246935a95e3e851593
boldPro <probold777@gmail.com>
2024-12-26 13:12:46 -0500

    changed mobile navbar logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ export default function NavBar ({ name, setUser }) {
     name?
     <>
     <nav className = "navBar navbar fixed-top admin-navbar">
-        <div>
+        <div className="navbar-logo">
           <Link to="/" className = "d-inline-block align-text-top">
           <img src ="https://i.imgur.com/3WSXIEd.png" height = "30px" className = "brand-img" alt="Salem Crown Apartments Logo"></img> 
           <span className="navbar-brand">Salem Crown Apartments</span> 
